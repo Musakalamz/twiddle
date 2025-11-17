022b5a04bfcc96a1c68e687196b7680b21374bdc
Musakalamz <ogunsolumusa@gmail.com>
2025-11-17 13:41:50 +0100

    Feat: Create the login page

--- twiddle-app/app/(auth)/login/[[...login]]/page.tsx
+++ twiddle-app/app/(auth)/login/[[...login]]/page.tsx
@@ -1,9 +1,9 @@
-import { SignUp } from "@clerk/nextjs";
+import { SignIn } from "@clerk/nextjs";
 
 export default function Page() {
   return (
     <>
-      <SignUp />
+      <SignIn />
     </>
   );
 }
