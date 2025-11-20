778e407aa54347191b428164ed172f51ed465f23
Musakalamz <ogunsolumusa@gmail.com>
2025-11-20 10:57:20 +0100

    feat: Creating the root layout

--- twiddle-app/app/(root)/layout.tsx
+++ twiddle-app/app/(root)/layout.tsx
@@ -3,6 +3,7 @@ import { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "../globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
+import { currentUser } from "@clerk/nextjs/server";
 
 export const metadata: Metadata = {
   title: "Twiddle",
@@ -14,20 +15,51 @@ const inter = Inter({
   subsets: ["latin"],
 });
 
-export default function RootLayout({
+export default async function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const user = await currentUser();
+
+  if (!user) {
+    return (
+      <>
+        <html lang="en">
+          <ClerkProvider>
+            <body>
+              <main className={`${inter.className} bg-black`}>
+                <div className="w-full flex justify-center items-center min-h-screen">
+                  {children}
+                </div>
+              </main>
+            </body>
+          </ClerkProvider>
+        </html>
+      </>
+    );
+  }
+
   return (
     <>
       <html lang="en">
         <ClerkProvider>
           <body>
             <main className={`${inter.className} bg-black`}>
-              <div className="w-full flex justify-center items-center min-h-screen">
-                {children}
-              </div>
+              <TopBar />
+
+              <main className="flex flex-row">
+                <LeftSideBar />
+
+                <section className="main-container">
+                  <div className="w-full flex justify-center items-center min-h-screen">
+                    {children}
+                  </div>
+                </section>
+                <RightSideBar />
+              </main>
+
+              <BottomBar />
             </main>
           </body>
         </ClerkProvider>
