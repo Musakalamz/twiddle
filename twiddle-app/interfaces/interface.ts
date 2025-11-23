6d6f840140ce413d45fee8e26138884d7a1a723f
Musakalamz <ogunsolumusa@gmail.com>
2025-11-23 14:03:51 +0100

    feat: Creating the onboarding page, account info and its logic

--- twiddle-app/interfaces/interface.ts
+++ twiddle-app/interfaces/interface.ts
@@ -5,3 +5,21 @@ export interface CreateUserParams {
   name: string;
   image: string;
 }
+
+export interface CreateUserParams {
+  userId: string;
+  email: string;
+  username: string;
+  name: string;
+  image: string;
+}
+
+export interface updateUserParams {
+  userId: string;
+  email?: string;
+  username?: string;
+  name?: string;
+  bio?: string;
+  image?: string;
+  path?: string;
+}
--- twiddle-app/lib/actions/user.actions.ts
+++ twiddle-app/lib/actions/user.actions.ts
@@ -1,8 +1,9 @@
 "use server";
 
+import { revalidatePath } from "next/cache";
 import User from "../models/user.model";
 import { connectToDB } from "../mongoose";
-import { CreateUserParams } from "@/interfaces/interface";
+import { CreateUserParams, updateUserParams } from "@/interfaces/interface";
 
 export const createUser = async ({
   userId,
@@ -22,5 +23,47 @@ export const createUser = async ({
     });
   } catch (err: any) {
     throw new Error(`Failed to create user: ${err.message}`);
+  }
+};
+
+export const fetchUser = async (userId: string) => {
+  try {
+    connectToDB();
+
+    return await User.findOne({
+      id: userId,
+    });
+  } catch (err: any) {
+    throw new Error(`Failed to fetch user: ${err.message}`);
+  }
+};
+
+export const updateUser = async ({
+  userId,
+  name,
+  email,
+  username,
+  bio,
+  path,
+  image,
+}: updateUserParams): Promise<void> => {
+  try {
+    connectToDB();
+    await User.findOneAndUpdate(
+      { id: userId },
+      {
+        name,
+        email,
+        username,
+        bio,
+        path,
+        image,
+        onboarded: true,
+      }
+    );
+
+    if (path === "/profile/edit") revalidatePath(path);
+  } catch (err: any) {
+    throw new Error(`Failed to update user info: ${err.message}`);
   }
 };
