c6010b2d14c21949b5dffcd65a7987c545de5294
Musakalamz <ogunsolumusa@gmail.com>
2025-11-24 03:45:11 +0100

    Removing members from a group

--- twiddle-app/lib/actions/group.actions.ts
+++ twiddle-app/lib/actions/group.actions.ts
@@ -1,5 +1,6 @@
 "use server";
 
+import { FilterQuery, SortOrder } from "mongoose";
 import Group from "../models/group.model";
 import User from "../models/user.model";
 import { connectToDB } from "../mongoose";
@@ -79,6 +80,41 @@ export const addMemberToGroup = async (groupId: string, memberId: string) => {
   } catch (error) {
     // Handle any errors
     console.error("Error adding member to group:", error);
+    throw error;
+  }
+};
+
+export const removeUserFromGroup = async (userId: string, groupId: string) => {
+  try {
+    connectToDB();
+
+    const userIdObject = await User.findOne({ id: userId }, { _id: 1 });
+    const groupIdObject = await Group.findOne({ id: groupId }, { _id: 1 });
+
+    if (!userIdObject) {
+      throw new Error("User not found");
+    }
+
+    if (!groupIdObject) {
+      throw new Error("Group not found");
+    }
+
+    // Remove the user's _id from the members array in the group
+    await Group.updateOne(
+      { _id: groupIdObject._id },
+      { $pull: { members: userIdObject._id } }
+    );
+
+    // Remove the group's _id from the groups array in the user
+    await User.updateOne(
+      { _id: userIdObject._id },
+      { $pull: { groups: groupIdObject._id } }
+    );
+
+    return { success: true };
+  } catch (error) {
+    // Handle any errors
+    console.error("Error removing user from group:", error);
     throw error;
   }
 };
