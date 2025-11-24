1a7ca663d1d5e7e737ef2363cb27dca582b2d6d6
Musakalamz <ogunsolumusa@gmail.com>
2025-11-24 17:31:09 +0100

    Creating the HomePage of the app

--- twiddle-app/app/(root)/page.tsx
+++ twiddle-app/app/(root)/page.tsx
@@ -1,9 +1,16 @@
 import LandingPage from "@/components/shared/LandingPage";
+
+import { fetchTweets, isTweetByUser } from "@/lib/actions/tweet.actions";
+import { fetchUser } from "@/lib/actions/user.actions";
 import { currentUser } from "@clerk/nextjs/server";
+import { redirect } from "next/navigation";
 
-export default async function Home() {
+export default async function Home({
+  searchParams,
+}: {
+  searchParams: { [key: string]: string | undefined };
+}) {
   const user = await currentUser();
-
   if (!user) {
     return (
       <>
@@ -12,12 +19,13 @@ export default async function Home() {
     );
   }
 
-  return (
-    <main>
-      <h1 className="text-center">
-        Twiddle
-        {/* <LandingPage /> */}
-      </h1>
-    </main>
+  const userInfo = await fetchUser(user.id);
+  if (!userInfo?.onboarded) redirect("/onboarding");
+
+  const result = await fetchTweets(
+    searchParams.page ? +searchParams.page : 1,
+    3
   );
+
+  return <></>;
 }
